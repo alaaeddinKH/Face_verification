108e2fbd57243bf1eb89f12fcc7be7301d2d8ce7
ALAAEDDİN  KHEİTİ <103529192+alaaeddinKH@users.noreply.github.com>
2025-01-05 18:25:37 +0300

    commit first

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.write("## Verificate your employee's face")
 st.write("### DSM 5014 Derin Öğrenme Yöntemleri ve Uygulamaları")
 
 st.write(
-    ":dog: Try implementing face verification using one-shot learning and test the model's ability to learn from a single instance. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) and create by Alaaeddin:grin:"
+    ":dog: Try implementing face verification using one-shot learning and test the model's ability to learn from a single instance. This code is open source and available [here](https://github.com/alaaeddinKH/Face_verification) and create by Alaaeddin:grin:"
 )
 st.sidebar.write("## Upload and download :gear:")
 
